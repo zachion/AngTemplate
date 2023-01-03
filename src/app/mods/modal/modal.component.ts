2cb42fee649e875a40260ea1eaa5b2cf1e15c53e
Zacharias Ioannou <zacharias.ioannou@epicstudios.org>
2023-01-03 15:00:47 +0200

    added child to parent communication

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
+    
+  @Output() close = new EventEmitter();
+
   constructor(private el: ElementRef) {
     //console.log(this.el.nativeElement);
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
   }
+
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick(){
+    this.close.emit();
+  }
+  
 }
--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent {
+  isModalOpen: boolean = false;
 
+  onClick() {
+    this.isModalOpen = !this.isModalOpen;
+  }
 }
