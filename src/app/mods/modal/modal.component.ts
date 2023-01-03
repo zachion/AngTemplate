1c84290bd29c7abe206aae42044090bf5ff2becc
Zacharias Ioannou <zacharias.ioannou@epicstudios.org>
2023-01-03 12:21:41 +0200

    initial commit

---
+++ src/app/mods/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit {
+  constructor(private el: ElementRef) {
+    //console.log(this.el.nativeElement);
+  }
+
+  ngOnInit() {
+    document.body.appendChild(this.el.nativeElement);
+  }
+}
