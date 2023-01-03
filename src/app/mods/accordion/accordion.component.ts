1f9a36ed1857b6499191b1a0438171af0a3f482f
Zacharias Ioannou <zacharias.ioannou@epicstudios.org>
2023-01-03 16:48:28 +0200

    implemented reusable accordion component

---
+++ src/app/mods/accordion/accordion.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css'],
+})
+export class AccordionComponent {
+  @Input() data: any = [];
+  openedItemIndex: number = 0;
+
+  onClick(index: number) {
+    if (index === this.openedItemIndex) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = index;
+    }
+  }
+}
--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class ModsHomeComponent {
   isModalOpen: boolean = false;
+  items = [
+    {
+      title: 'Why is the sky blue',
+      content:
+        'The sky is blue because it is always blue and it never gets gray in greece',
+    },
+    {
+      title: 'What is the situation in Greece',
+      content: 'The situation in greece is and always has been amazing!',
+    },
+    {
+      title: 'How dark is the night',
+      content: 'It is really dark becuase it is winter',
+    },
+    {
+      title: 'There are three types of pandomimas',
+      content: 'Silent, spoken and mimic',
+    },
+    {
+      title: 'Future telling is not a thing',
+      content: 'There are only those things that matter to us.',
+    },
+  ];
 
   onClick() {
     this.isModalOpen = !this.isModalOpen;
